ad0dd295fca4eadbeebccac6ba9387192f832ddc
Lucas Pedroletti <ens20lpi@cs.umu.se>
2026-02-18 12:38:03 +0100

    Updated credits

--- src/resources/MainMenuHandler.java
+++ src/resources/MainMenuHandler.java
@@ -63,6 +63,6 @@ public class MainMenuHandler {
         return getMenuString() + "\n [ SETTINGS ]\n No settings yet";
     }
     public static String getCreditsText() {
-        return getMenuString() + "\n [ CREDITS ]\n github.com/pedroletti\n";
+        return getMenuString() + "\n [ CREDITS ]\n github.com/pedroletti\n\n [ ASCII ART ]\n Modified & Used art from\n asciiart.eu";
     }
 }
